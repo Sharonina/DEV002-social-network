82792735b4393ff5adc772ba538ff2d16fac723b
Ingrid G <ing.gape18@gmail.com>
2023-01-13 19:22:13 -0500

    validado el campo de creacion de post - no se envian post vacios

--- src/logic/creacionPostLogica.js
+++ src/logic/creacionPostLogica.js
@@ -8,14 +8,14 @@ export const creacionPostLogica = (contenedor) => {
     ladraloBtn.addEventListener('click', () => {
         const valorPost = ingresoPost.value;
         const userUid = window.localStorage.getItem('uid');
-
-        const posts = collection(database, 'posts');
-        Promise.all([
-            addDoc(collection(posts, auth.currentUser.uid, 'userPosts'), {
-                userUid, valorPost,
-            }),
-
-        ]);
+        if (ingresoPost.value.length > 0) {
+            const posts = collection(database, 'posts');
+            Promise.all([
+                addDoc(collection(posts, auth.currentUser.uid, 'userPosts'), {
+                    userUid, valorPost,
+                }),
+            ]);
+        }
         ingresoPost.value = '';
     });
 };
