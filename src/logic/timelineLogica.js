e8073996a578e679348e78dfb490a73064718251
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2023-01-17 22:13:43 -0500

    Añadiendo funcionalidad al boton de likes

--- src/logic/timelineLogica.js
+++ src/logic/timelineLogica.js
@@ -1,7 +1,7 @@
 import { onSnapshot, collection, query, where, getDocs, doc, getDoc } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
 import { ref, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js';
-import { database, auth, storage, currentUser, deletePost, getPostData } from '../firebase/configuracionFirebase.js';
+import { database, auth, storage, currentUser, deletePost, getPostData2, likePost, dislikePost } from '../firebase/configuracionFirebase.js';
 import Timeline from '../templates/Timeline.js';
 
 export const timelineLogica = (contenedor) => {
@@ -11,11 +11,14 @@ export const timelineLogica = (contenedor) => {
     // consultar texto del post
     const userUid = window.localStorage.getItem('uid');
     const subColRef = collection(database, 'usuarios', userUid, 'userPosts');
-    
+    // const array = []
+    // const docId = []
     onSnapshot(subColRef, (querySnapshot) => {
         postPublicado.innerHTML = '';
         querySnapshot.forEach((doc) => {
             const post = doc.data();
+            // array.push(post);
+            // docId.push(doc.id);
             const fechaPublicacion = new Date().toLocaleDateString('en-us', { weekday: 'long', month: 'long', day: 'numeric' });
             postPublicado.innerHTML += `
                 <section class = 'postIndividual'>
@@ -38,17 +41,42 @@ export const timelineLogica = (contenedor) => {
                         <img class ='imagenDelPost' src='' alt = ''/>
                     </figure>
                     <div class='postBotones'>
-                        <button class = 'likes'>
-                            <img class='likeImage' src='./assets/heart.png' alt="foto de like a post"/>
+                        <button class = 'likes' data-uid='${doc.id}'>
+                            <img class='likeImage' data-uid='${doc.id}' src='./assets/heart.png' alt="foto de like a post"/>
                         </button>
-                        <button class = 'contadorLikes'>10</button>
+                        <button class = 'contadorLikes'>${post.arrayUsersLikes.length}</button>
                     </div>
                 </section>
             `;
+            // if (post.arrayUsersLikes.length === 0) {
+            //     contadorLikes.innerHTML = '';
+            // }
+            // Boton de likes y dislikes
+            // const likeButton = postPublicado.querySelectorAll('.likes');
+            // console.log(likeButton);
+            // likeButton.forEach((like) => {
+            //     like.addEventListener('click', () => {
+            //         console.log('me dieron click');
+            //         console.log(doc.id);
+            //         console.log(post.arrayUsersLikes);
+            //         const currentUserLike = post.userUid;
+            //         const idLikeButton = doc.id;
+            //         if (!post.arrayUsersLikes.includes(currentUserLike)) {
+            //             console.log('le sumaron 1 :D ');
+            //             const likes = (post.amountLikes) + 1;
+            //             likePost(idLikeButton, likes, currentUserLike);
+            //         } else {
+            //             console.log('le restaron 1 :(');
+            //             const likes = (post.amountLikes) - 1;
+            //             dislikePost(idLikeButton, likes, currentUserLike);
+            //         }
+            //     });
+            // });
         });
         const borrarPostBtn = postPublicado.querySelectorAll('.borrarPost');
         borrarPostBtn.forEach(btn => {
             btn.addEventListener('click', ({ target: { dataset } }) => {
+                console.log(dataset.uid);
                 deletePost(dataset.uid);
             });
         });
@@ -58,5 +86,61 @@ export const timelineLogica = (contenedor) => {
                 
             });
         });
+        // querySnapshot.forEach((doc) => {
+        // console.log(array[0]);
+        // console.log(docId[0]);
+        // Boton de likes y dislikes
+        const likeButton = postPublicado.querySelectorAll('.likes');
+        likeButton.forEach((btn) => {
+            btn.addEventListener('click', ({ target: { dataset } }) => {
+                console.log(dataset.uid);
+                const currentUserLike = auth.currentUser.uid;
+                const idLikeButton = dataset.uid;
+                getPostData2(idLikeButton)
+                    .then((document) => {
+                        const post = document.data();
+                        console.log(post);
+                        if (!post.arrayUsersLikes.includes(currentUserLike)) {
+                            const likes = (post.amountLikes) + 1;
+                            likePost(idLikeButton, likes, currentUserLike);
+                        } else {
+                            const likes = (post.amountLikes) - 1;
+                            dislikePost(idLikeButton, likes, currentUserLike);
+                        }
+                    })
+                    .catch(() => {
+                    });
+            });
+        });
+        // const contadorLikes = contenedor.querySelectorAll('.contadorLikes');
+        // contadorLikes.forEach((btn) => {
+        //     console.log((btn.innerHTML === 0) ? 'lala');
+        // });
+        // console.log(likeButton);
+        // likeButton.forEach((like) => {
+        //     like.addEventListener('click', ({ target: { dataset } }) => {
+        //         console.log('me dieron click');
+        //         console.log(docId[0]);
+        //         console.log(array[0].arrayUsersLikes);
+        //         const currentUserLike = auth.currentUser.userUid;
+        //         let idLikeButton = docId[0];
+        //         getPostData2(idLikeButton)
+        //             .then((document) => {
+        //                 const post = document.data();
+        //                 console.log(post);
+        //                 console.log('Si pasa');
+        //                 if (!post.arrayUsersLikes.includes(currentUserLike)) {
+        //                     console.log('le sumaron 1 :D ');
+        //                     const likes = (post.amountLikes) + 1;
+        //                     likePost(idLikeButton, likes, currentUserLike);
+        //                 } else {
+        //                     console.log('le restaron 1 :(');
+        //                     const likes = (post.amountLikes) - 1;
+        //                     dislikePost(idLikeButton, likes, currentUserLike);
+        //                 }
+        //             });
+        //         idLikeButton = '';
+        //     });
+        // });
     });
 };
